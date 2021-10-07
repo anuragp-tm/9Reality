f1ca6b6947a99b4948f9a1e8c6eb9a486a9d7282
babloo_kumar <babloo.kumar@epicor.com>
2021-10-07 10:34:03 +0530

    Added code changes

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,10 +1,13 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
-
 import { HomePageRoutingModule } from './home-routing.module';
+
+import { HttpClientModule } from '@angular/common/http';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
 
 @NgModule({
@@ -12,8 +15,13 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    HttpClientModule,
+    NgxDatatableModule
   ],
-  declarations: [HomePage]
+  declarations: [
+    HomePage
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HomePageModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ViewEncapsulation } from '@angular/core';
+
+export interface Data {
+  movies: string;
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  encapsulation: ViewEncapsulation.None
 })
+
 export class HomePage {
+  public data: Data;
+  public columns: any;
+  public rows: any;
 
-  constructor() {}
+  constructor(
+    private http: HttpClient
+  ) {
+    this.columns = [
+      { name: 'Name' },
+      { name: 'Company' },
+      { name: 'Genre' },
+      { name: 'Test' }
+    ];
 
+    this.http.get<Data>('../../assets/sample-data/movies.json')
+      .subscribe((res) => {
+        console.log(res);
+        this.rows = res.movies;
+      });
+  }
 }
